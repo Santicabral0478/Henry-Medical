fc65e7155dcd2b9f1739d4f52699fd5e913d9943
Santicabral0478 <148177522+Santicabral0478@users.noreply.github.com>
2024-02-29 11:34:45 -0300

    Delete src directory

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const nombre1:string = "Carlin";
-
-const frase2:string = "Hola, como estas. Me llamo"
-
-const funcionEjemplo = (nombre: string, frase: string): string =>{
-    return (`${frase} ${nombre}`);
-}
-
-console.log(funcionEjemplo(nombre1, frase2));
